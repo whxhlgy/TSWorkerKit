855518555a26602f2f7ecb00ac1985c4693058dc
Junjie-Zhong <zjj476778843@gmail.com>
2024-02-27 13:14:51 +0800

    set operation done

--- test/Cook.ts
+++ test/Cook.ts
@@ -6,34 +6,38 @@ export class Cook extends WebWorker {
     // Class definition
     source =`
 // Worker thread JavaScript code
-console.log('Cooker start!');
 class __Desk {
-    public static food_flag: number = 0;
-
-    public static count: number = 10;
+    static food_flag = 0;
+    static count = 10;
+    static className = 'Desk';
 }
-
-let workerPort: MessagePort;
+let workerPort;
 self.onmessage = ev => {
     if (ev.data.command === 'connect') {
         workerPort = ev.ports[0];
         console.log('connected!');
     }
 };
-var Desk = new Proxy(__Desk,
-    {
-        get: function(target, propKey, receiver) {
-            // TODO
-        },
-        set(target, p, newValue, receiver): boolean {
-            target[p as keyof typeof target] = newValue
-            workerPort.postMessage({ command: 'set', key: target, value: {...target, p: newValue }})            
-            return true;
+
+var Desk = new Proxy(__Desk, {
+    get: function(target, propKey, receiver) {
+        // TODO: Implement logic if needed
+    },
+    set: function(target, propKey, newValue, receiver) {
+        target[propKey] = newValue;
+        console.log('set value');
+        if (workerPort) {
+            workerPort.postMessage({ command: 'set', key: target.className, value: { ...target } });
         }
+        return true;
     }
-);
+});
 console.log('Cooker start!');
-Desk.food_flag = 1
+// 怎么让Worker在main thread 运行好之前，不要执行set和get操作
+// TODO()
+setTimeout(() => {
+    Desk.food_flag = 1;
+}, 2000);
 `;
 }
 
