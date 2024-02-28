03a1c20e29eedb0bdbf7619f81004e50760b981d
Junjie-Zhong <zjj476778843@gmail.com>
2024-02-28 12:22:59 +0800

    add get operation test

--- test/Cook.ts
+++ test/Cook.ts
@@ -13,7 +13,7 @@ class __Desk {
 }
 var Desk = new Proxy(__Desk, {
     get: function(target, propKey, receiver) {
-        // TODO: Implement logic if needed
+        // TODO
     },
     set: function(target, propKey, newValue, receiver) {
         target[propKey] = newValue;
@@ -25,10 +25,39 @@ var Desk = new Proxy(__Desk, {
     }
 });
 console.log('Cooker start!');
-// 怎么让Worker在main thread 运行好之前，不要执行set和get操作
-// TODO()
 Desk.food_flag = 1;
 `;
 }
-const worker = new Cook(); // Assuming Cook is a subclass of WebWorker
-worker.start();
+export class Customer extends WebWorker {
+
+    __captured_cvs = { 'Desk': 'Desk' }
+    // Class definition
+    source =`
+// Worker thread JavaScript code
+class __Desk {
+    static food_flag = 0;
+    static count = 10;
+    static className = 'Desk';
+}
+var Desk = new Proxy(__Desk, {
+    get: function(target, propKey, receiver) {
+        // TODO
+    },
+    set: function(target, propKey, newValue, receiver) {
+        target[propKey] = newValue;
+        console.log('set value');
+        if (workerPort) {
+            workerPort.postMessage({ command: 'set', key: target.className, value: { ...target } });
+        }
+        return true;
+    }
+});
+console.log('Customer start!');
+while (Desk.food_flag === 0);
+console.log(Desk.food_flag);
+`;
+}
+const cooker = new Cook(); // Assuming Cook is a subclass of WebWorker
+const customer = new Customer(); // Assuming Cook is a subclass of WebWorker
+cooker.start();
+customer.start();
